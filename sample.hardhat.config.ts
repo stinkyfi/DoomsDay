a519c78fc273d6838cd1a0a0227f8d973cc08094
stinkyfi <stinkyzeroex@gmail.com>
2022-11-01 17:39:40 -0700

    simplify hardhat config for workflow

--- sample.hardhat.config.ts
+++ sample.hardhat.config.ts
@@ -4,11 +4,6 @@ import "@nomicfoundation/hardhat-chai-matchers"
 import 'solidity-coverage'
 import 'hardhat-docgen'
 
-const PRIVATE_KEY = ""
-const ALCHEMY_API_KEY = ""
-const COINMARKETCAP_API_KEY = ""
-const ETHERSCAN_API_KEY = ""
-
 const config: HardhatUserConfig = {
   solidity: "0.8.15",
   networks: {
@@ -20,14 +15,6 @@ const config: HardhatUserConfig = {
       url: "http://localhost:8545",
       chainId: 1337,
       allowUnlimitedContractSize: true,
-    },
-    mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${PRIVATE_KEY}`]
-    },
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${PRIVATE_KEY}`]
     }
   },
   gasReporter: {
@@ -36,7 +23,7 @@ const config: HardhatUserConfig = {
     gasPrice: 6,    
     enabled: true,
     showTimeSpent: true,    
-    coinmarketcap: `${COINMARKETCAP_API_KEY}`
+    coinmarketcap: ``
   },
   // etherscan: {
   //   apiKey: `${ETHERSCAN_API_KEY}`
